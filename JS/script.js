62a650d2c41dc0ef7c5dcc2ca1127b4daaab7eb3
Aisha Alselwi <123330794+AlselwiAisha@users.noreply.github.com>
2023-07-24 17:28:12 +0300

    Fix linter errors

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,28 @@
 /* mobile menu */
 const menu = document.querySelector('#menu-icon');
 const nav = document.querySelector('.nav-header');
-const nav_bar = document.querySelector('.nav-bar');
+const navBar = document.querySelector('.nav-bar');
 const links = document.querySelector('.navbar-links');
-const img_ = document.querySelector('.menuImage1');
-const img_2 = document.querySelector('.menuImage2');
+const img0 = document.querySelector('.menuImage1');
+const img2 = document.querySelector('.menuImage2');
 const img1 = document.querySelector('.headerImage');
 
 menu.onclick = () => {
   menu.classList.toggle('bx-x');
   links.classList.toggle('open-menu');
-  img_.classList.toggle('view-img');
-  img_2.classList.toggle('view-img');
+  img0.classList.toggle('view-img');
+  img2.classList.toggle('view-img');
   img1.classList.toggle('h-img');
   nav.classList.toggle('heder-position');
-  nav_bar.classList.toggle('view-img');
+  navBar.classList.toggle('view-img');
 };
 
 window.onscroll = () => {
   menu.classList.remove('bx-x');
   links.classList.remove('open-menu');
-  img_.classList.remove('view-img');
-  img_2.classList.remove('view-img');
+  img0.classList.remove('view-img');
+  img2.classList.remove('view-img');
   img1.classList.remove('h-img');
   nav.classList.remove('heder-position');
-  nav_bar.classList.remove('view-img');
+  navBar.classList.remove('view-img');
 };
