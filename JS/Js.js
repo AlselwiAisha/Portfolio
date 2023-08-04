099d7ca0924eeebba5b88bd3ea23fd82ee60975d
Aisha Alselwi <123330794+AlselwiAisha@users.noreply.github.com>
2023-08-04 16:41:50 +0300

    Update Js.js

--- JS/Js.js
+++ JS/Js.js
@@ -1,1 +1,23 @@
 
+ function fib(num) {
+  let n1=1,
+  temp=0,arr=[];
+  let n2=0;
+  let oddnumsum=0 ;
+while(temp<=num)
+{
+  arr.push(temp);
+  n2=n1;
+  n1=temp;
+  temp=n2+n1;
+}
+for(let i=0;i<arr.length;i++)
+{
+  if(arr[i]%2===1)
+    {
+      oddnumsum +=arr[i];
+    } 
+}
+return(oddnumsum);
+   console.log(e);
+}
